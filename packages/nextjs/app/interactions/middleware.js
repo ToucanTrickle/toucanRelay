61836c8e454874e51464c851cbedfab4d6670886
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-04-30 16:54:31 +0530

    patch interactions endpoint

--- packages/nextjs/app/interactions/middleware.js
+++ packages/nextjs/app/interactions/middleware.js
@@ -5,7 +5,7 @@ export function middleware(req) {
   const signature = req.get("X-Signature-Ed25519");
   const timestamp = req.get("X-Signature-Timestamp");
 
-  const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+  const isValidRequest = verifyKey(buf, signature, timestamp, process.env.PUBLIC_KEY);
   if (!isValidRequest) {
     res.status(401).send("Bad request signature");
     throw new Error("Bad request signature");
